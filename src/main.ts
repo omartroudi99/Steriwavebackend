bb0c96b6890196cc233c637831caf7d4915a0f05
omartroudi99 <omartroudi88@gmail.com>
2026-03-06 10:55:08 +0100

    fix: use process.env.PORT for Railway

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors({
     origin: '*', 
   });
-await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // ✅ process.env.PORT
 }
 bootstrap();
